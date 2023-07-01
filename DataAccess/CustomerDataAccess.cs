629a548e052c46a639c6488787000aedfe82a094
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2023-07-02 03:29:03 +0430

    Init CustomerDataAccess.cs & EmployeeDataAccess.cs

---
+++ DataAccess/CustomerDataAccess.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess.Models;
+
+namespace DataAccess;
+
+public class CustomerDataAccess
+{
+    public List<Customer> Customers { get; set; } = new List<Customer>();
+
+    public CustomerDataAccess()
+    {
+        ReadCustomers();
+    }
+
+    private void ReadCustomers()
+    {
+        var customer1 = new Customer()
+        {
+            id = 1,
+            name = "علی",
+            lastName = "حسینی",
+            password = "sdfj1523",
+            nationalCode = "0036984572",
+            email = "alihos@gmail.com",
+            phoneNo = "09125654856",
+            userName = "jdsfkj"
+        };
+        
+        var customer2 = new Customer()
+        {
+            id = 2,
+            name = "غلامی",
+            lastName = "فاطمی",
+            password = "kjfj1541",
+            nationalCode = "0025754572",
+            email = "gholamfat@gmail.com",
+            phoneNo = "09195474856",
+            userName = "werew"
+        };
+        
+        Customers.Add(customer1);
+        Customers.Add(customer2);
+    }
+
+    private void AddCustomer(Customer customer)
+    {
+        Customers.Add(customer);
+    }
+
+    public void RemoveCustomer(int id)
+    {
+        var temp = Customers.First(x => x.id == id);
+        Customers.Remove(temp);
+    }
+
+    public void EditCustomer(Customer customer)
+    {
+        var temp = Customers.First(x => x.id == customer.id);
+        var index = Customers.IndexOf(temp);
+        Customers[index] = customer;
+    }
+
+    public int GetNextId()
+    {
+        var index = Customers.Any() ? Customers.Max(x => x.id) + 1 : 1;
+        return index;
+    }
+}
---
+++ DataAccess/EmployeeDataAccess.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Models;
+
+namespace DataAccess;
+
+public class EmployeeDataAccess
+{
+    public List<Employee> Employees { get; set; } = new List<Employee>();
+
+    public EmployeeDataAccess()
+    {
+        ReadEmployees();
+    }
+
+    private void ReadEmployees()
+    {
+        var emp1 = new Employee()
+        {
+            id = 1,
+            name = "علی",
+            lastName = "حسینی",
+            password = "sdfj1523",
+            personalIdNo = "40044",
+            userName = "jdsfkj"
+        };
+        
+        var emp2 = new Employee()
+        {
+            id = 2,
+            name = "غلامی",
+            lastName = "فاطمی",
+            password = "kjfj1541",
+            personalIdNo = "40172",
+            userName = "werew"
+        };
+        
+        Employees.Add(emp1);
+        Employees.Add(emp2);
+    }
+
+    private void AddEmployee(Employee employee)
+    {
+        Employees.Add(employee);
+    }
+
+    public void RemoveEmployee(int id)
+    {
+        var temp = Employees.First(x => x.id == id);
+        Employees.Remove(temp);
+    }
+
+    public void EditEmployee(Employee employee)
+    {
+        var temp = Employees.First(x => x.id == employee.id);
+        var index = Employees.IndexOf(temp);
+        Employees[index] = employee;
+    }
+
+    public int GetNextId()
+    {
+        var index = Employees.Any() ? Employees.Max(x => x.id) + 1 : 1;
+        return index;
+    }
+}
