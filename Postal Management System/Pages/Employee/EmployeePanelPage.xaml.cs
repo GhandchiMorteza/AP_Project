b486258aea43042d197bf2ed1b576685cc5fdf97
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2023-07-03 04:20:21 +0430

    EmployeePanel init

--- Postal Management System/Pages/Employee/EmployeePanelPage.xaml.cs
+++ Postal Management System/Pages/Employee/EmployeePanelPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Postal_Management_System.Pages.Employee;
 
@@ -7,5 +9,10 @@ public partial class EmployeePanelPage : Page
     public EmployeePanelPage()
     {
         InitializeComponent();
+    }
+
+    private void RegisterCustomerBtn_OnClick(object sender, RoutedEventArgs e)
+    {
+        EmployeePanelFrame.Navigate(new Uri("/Pages/Employee/RegisterCustomerPage.xaml", UriKind.Relative));
     }
 }
---
+++ Postal Management System/Pages/Employee/RegisterCustomerPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Postal_Management_System.Pages.Employee;
+
+public partial class RegisterCustomerPage : Page
+{
+    public RegisterCustomerPage()
+    {
+        InitializeComponent();
+    }
+}
